9f64d295bb46ec5fef0a4c38d7580940dfac25fb
novikovVan <novikov123vany@gmail.com>
2024-05-01 10:16:36 +0300

    Add percesten layer

--- Notes.Backend/Notes.Domain/Note.cs
+++ Notes.Backend/Notes.Domain/Note.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Notes.Domain
 {
-    internal class Note
+    public class Note
     {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Edit { get; set; }
     }
 }
